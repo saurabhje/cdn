501902e2ba6ab8ba4bfa933ebec1634eb3d7af63
saurabhje <work.saurabh@outlook.com>
2025-04-21 02:09:46 +0530

    Add: Edge-server, caching works

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Content Delivery Network Simulation
 > SaurabhJe
+
 Content Delivery Network simulation in Node.js with custom caching, regional routing, and multi-node deployment using Docker and Azure/DigitOcean.
+
+## Step the simulation works
+1. Creating the origin server, which can be running at some PORT like 3000.
+2. Creating multiple edge server instances, can be done with using env and ID.
--- origin-server.js
+++ origin-server.js
@@ -3,17 +3,18 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
-// emulating __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Define the paths to the static files
-const htmlPath = join(__dirname, 'static', 'index.html');
-const cssPath = join(__dirname, 'static', 'style.css');
+const indexHtml = join(__dirname, 'public', 'index.html');
+const cssPath = join(__dirname, 'public', 'style.css');
+const reqHtml = join(__dirname, 'public', 'request.html')
 
+//creating the server instance
 const server = http.createServer((req, res) => {
   if (req.url === '/' || req.url === '/index.html') {
-    fs.readFile(htmlPath, 'utf-8', (err, data) => {
+    fs.readFile(indexHtml, 'utf-8', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Server error');
@@ -22,6 +23,17 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
+  }
+  else if (req.url === '/req.html'){
+    fs.readFile(reqHtml, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    })
   }
   else if (req.url === '/style.css') {
     fs.readFile(cssPath, (err, data) => {
